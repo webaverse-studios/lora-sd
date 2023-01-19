9b8e09eab0de6b73a1675720f0874255fbd5bf0b
Arpan Tripathi <tripathiarpan20@gmail.com>
2023-01-19 15:10:47 +0000

    partial train pythonization

--- base.py
+++ base.py
@@ -17,17 +17,19 @@ def train_lora(MODEL_NAME = "stable-diffusion-2-1-base",\
     TRAIN_BATCH_SIZE = 1
     GRAD_ACC_STEPS = 1
 
-    # lora_train_args = lora_train.parse_args(input_args=['--pretrained_model_name_or_path', MODEL_NAME,\
-    #     '--instance_data_dir', INSTANCE_DATA_DIR, '--output_dir', OUTPUT_DIR, '--instance_prompt', KEYWORD, '--train_text_encoder',\
-    #     '--use_8bit_adam', '--resolution', RESOLUTION, '--mixed_precision', fp_16_arg, '--train_batch_size', TRAIN_BATCH_SIZE,\
-    #     '--gradient_accumulation_steps', GRAD_ACC_STEPS, '--learning_rate', UNET_LR, '--learning_rate_text', TEXT_ENC_LR, \
-    #     '--color_jitter', '--lr_scheduler', 'constant', '--lr_warmup_steps', 0, '--max_train_steps', TRAIN_STEPS])
+    lora_train_args = lora_train.parse_args(input_args=['--pretrained_model_name_or_path', MODEL_NAME,\
+        '--instance_data_dir', INSTANCE_DATA_DIR, '--output_dir', OUTPUT_DIR, '--instance_prompt', KEYWORD, '--train_text_encoder',\
+        '--use_8bit_adam', '--resolution', RESOLUTION, '--mixed_precision', fp_16_arg, '--train_batch_size', TRAIN_BATCH_SIZE,\
+        '--gradient_accumulation_steps', GRAD_ACC_STEPS, '--learning_rate', UNET_LR, '--learning_rate_text', TEXT_ENC_LR, \
+        '--color_jitter', '--lr_scheduler', 'constant', '--lr_warmup_steps', '0', '--max_train_steps', TRAIN_STEPS])
 
-    # lora_train.main(lora_train_args)
+    lora_train.main(lora_train_args)
 
-    os.system( f"python {'lora_train.py'} --pretrained_model_name_or_path={MODEL_NAME} --instance_data_dir={INSTANCE_DATA_DIR}\
-    --output_dir={OUTPUT_DIR} --instance_prompt='{KEYWORD}' --train_text_encoder --use_8bit_adam --resolution={RESOLUTION} --mixed_precision={fp_16_arg} --train_batch_size={TRAIN_BATCH_SIZE} --gradient_accumulation_steps={GRAD_ACC_STEPS} \
-    --learning_rate={UNET_LR} --learning_rate_text={TEXT_ENC_LR} --color_jitter --lr_scheduler='constant' --lr_warmup_steps=0 --max_train_steps={TRAIN_STEPS}")
+
+
+    # os.system( f"python {'lora_train.py'} --pretrained_model_name_or_path={MODEL_NAME} --instance_data_dir={INSTANCE_DATA_DIR}\
+    # --output_dir={OUTPUT_DIR} --instance_prompt='{KEYWORD}' --train_text_encoder --use_8bit_adam --resolution={RESOLUTION} --mixed_precision={fp_16_arg} --train_batch_size={TRAIN_BATCH_SIZE} --gradient_accumulation_steps={GRAD_ACC_STEPS} \
+    # --learning_rate={UNET_LR} --learning_rate_text={TEXT_ENC_LR} --color_jitter --lr_scheduler='constant' --lr_warmup_steps=0 --max_train_steps={TRAIN_STEPS}")
     
     return
 
@@ -53,8 +55,8 @@ def inference_lora_img2img(MODEL_NAME = "stable-diffusion-2-1-base",\
     INIT_IMG = 'local_img.png'
     ):
 
-    lora_img2img_args = inference_txt2img.parse_args(input_args=['--pretrained_model_name_or_path', MODEL_NAME,\
+    lora_img2img_args = inference_img2img.parse_args(input_args=['--pretrained_model_name_or_path', MODEL_NAME,\
         '--obj_prompt', KEYWORD, '--model_out_dir', OUTPUT_DIR, '--prompt', PROMPT, '--init_image', INIT_IMG])
 
-    images = inference_txt2img.main(lora_img2img_args)
+    images = inference_img2img.main(lora_img2img_args)
     return images
